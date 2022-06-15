71fc6010e09493cf2a0cd554fcd4d67eb91ee109
zyy <zyy2008@vip.qq.com>
2022-06-15 19:14:41 +0800

    提交

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,6 +1,9 @@
 import { FC } from "react";
-import { Flex } from "@taroify/core";
+import { Flex, Button } from "@taroify/core";
+import { Map, CoverView } from "@tarojs/components";
 import { Header } from "./components";
+import { Aim } from "@taroify/icons";
+import Taro from "@tarojs/taro";
 import "./index.scss";
 
 const Index: FC = () => {
@@ -16,9 +19,42 @@ const Index: FC = () => {
       <Flex.Item
         style={{
           flex: 1,
+          width: "100%",
+          position: "relative",
         }}
       >
-        123
+        <Map
+          style={{
+            width: "100%",
+            height: "100%",
+          }}
+          longitude={108.93}
+          latitude={34.27}
+          showLocation
+          scale={15}
+          id="myMap"
+        />
+        <CoverView className="location">
+          <Button
+            icon={
+              <Aim
+                style={{
+                  margin: 0,
+                }}
+                size={20}
+                onClick={() => {
+                  const mapCtx = Taro.createMapContext("myMap");
+                  mapCtx.moveToLocation({});
+                }}
+              />
+            }
+            shape="round"
+            style={{
+              width: 30,
+              height: 30,
+            }}
+          />
+        </CoverView>
       </Flex.Item>
     </Flex>
   );
