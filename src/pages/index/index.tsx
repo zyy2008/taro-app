c02e906747480ce2d1f8693e3cf5bf16683d06a0
zyy <zyy2008@vip.qq.com>
2022-06-30 11:33:41 +0800

    tijiao

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -100,8 +100,10 @@ const Index: React.FC = () => {
           markers={markers}
           onRegionChange={({ detail, causedBy, type }: any) => {
             if (type === "end") {
-              setScale(detail.scale);
-              setCenter(detail.centerLocation);
+              if (causedBy === "drag" || causedBy === "scale") {
+                setScale(detail.scale);
+                setCenter(detail.centerLocation);
+              }
             }
           }}
           onMarkerTap={(
